99bebadd1064556027a6a314ec3c38cbfb3655e7
Jiani Liu <liujiani505@gmail.com>
2023-06-10 16:09:24 -0400

    practiced subject

--- observables/src/app/app.component.ts
+++ observables/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  constructor(private userService: UserService) {}
+  private activatedSub: Subscription;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe( didActivate => {
+      this.userActivated = didActivate;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 }
