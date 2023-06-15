33a97d950fc169ba77a8683407e0af3f278b6cf7
Jiani Liu <liujiani505@gmail.com>
2023-06-14 23:47:44 -0400

    set and patch form data

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  // @ViewChild('username') userName: NgModel;
+  @ViewChild('username') userName: ElementRef;
   defaultQuestion = 'pet';
   defaultUsername = 'Jiani';
   answer = '';
@@ -15,6 +17,26 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+        // this can help you only overwrite certain field/fields in the form.
+        this.signupForm.form.patchValue({
+          userData:{
+            username: suggestedName,
+          }
+        })
+
+    // these are trying different approaches by using @viewchild to just the username input   
+    // this.userName.control.setValue(suggestedName);
+    // this.userName.nativeElement.value = suggestedName;
   }
   // onSubmit(form: NgForm){
   //   console.log(form)
