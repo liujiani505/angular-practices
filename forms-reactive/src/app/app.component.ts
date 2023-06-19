a003da34c4d9508cd22965144679beac6abdb777
jianiliu <liujiani505@gmail.com>
2023-06-19 16:12:43 -0400

    creating custom validators

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -13,9 +13,14 @@ export class AppComponent implements OnInit{
   
 
   ngOnInit(): void {
+
+    // In Javascript and Typescript, classes are special functions that are used to construct objects. When you call a class with the new keyword, it invokes the class's constructor method to create a new object. 
+    // When we pass our forbiddenNames method to FormControl's validator list, FormControl has no way of knowing that the function came from an instance of "AppComponent".
+    // When angular calls a validator function like forbiddenNames it doesn't call it as a method on AppComponent instance, instead it calls it as a standalone function, not attached to any object. In this situation, the value of `this` inside the function depends on the JavaScript strict mode, it's it's on, this will be undefined, it strick mode is not enabled, this will refer to the global object `window` or `gloabl`. This behavior of this in JavaScript can be quite tricky and is different from how this works in many other programming languages, where this inside a method always refers to the instance the method belongs to.
+    // Because we used .bind(this), `this` inside forbiddenNames still refers to the AppComponent instance. So this.forbiddenUsernames will correctly refer to the forbiddenUsernames array.
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -33,5 +38,15 @@ export class AppComponent implements OnInit{
   }
   getControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  // {[s:string]:boolean} is the return type of the function
+  // [s:string] specifices that the key names of the object will be type of string. brackets is used when you don't know the names of the key name, but you know the type of the values. s is just a placeholder.
+  // boolean specifies that the values associated with these keys will be of type boolean.
+  forbiddenNames(control: FormControl):{[s:string]:boolean}{
+    if( this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
