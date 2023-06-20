f227ac83ec1e0168c4bb9f590616a846b77e1258
jianiliu <liujiani505@gmail.com>
2023-06-20 11:45:59 -0400

    creating an async validator

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,7 +22,7 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([]),
@@ -52,4 +53,53 @@ export class AppComponent implements OnInit{
     }
     return null;
   }
+
+
+  // Asynchronous validators can return either a Promise or an Observable. They both serve similar purposes and can be used to handle async operations, but they work a bit differently:
+
+  //A Promise is an object representing the eventual completion or failure of an asynchronous operation. It is a one-time operation that either succeeds or fails.
+
+  //An Observable, part of the RxJS library, is more powerful. It is a stream of events over time that you can subscribe to. It's not just a one-time operation; it can be cancelled, retried, and it can trigger multiple events.
+
+  // a Promise is being used for simplicity, as it's only needed to run a single async operation. It's created to simulate a delay (1500ms) and then checks if the email equals 'test@test.com'. If it does, it resolves to { 'emailIsForbidden': true }, indicating the validation failed. If it doesn't, it resolves to null, indicating validation passed.
+
+  // This Promise is returned from the validator, and Angular subscribes to it. If it eventually resolves to an error object, Angular marks the form control as invalid, and if it resolves to null, Angular marks the form control as valid.
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any>{
+      const promise = new Promise<any>((resolve, reject) => {
+        setTimeout( ()=>{
+          if(control.value === 'test@test.com') {
+            resolve({'emailIsForbidden': true});
+          } else {
+            resolve(null);
+          }
+        },1500);
+      });
+      return promise;
+  }
 }
+
+
+// we can replace promise with observable, we can use the `of` function from RXJS to create an observable, and pipe and delay to simulate the delay
+
+// import { of } from 'rxjs';
+// import { delay } from 'rxjs/operators';
+// forbiddenEmails(control: FormControl): Observable<any> {
+//   return of(control.value === 'test@test.com' ? { 'emailIsForbidden': true } : null).pipe(delay(1500));
+// }
+
+
+// we can also imagine that we have an API endpoint that can check if an email is already taken.
+// forbiddenEmails(control: FormControl): Observable<any> {
+//  return this.http.get(`https://api.example.com/user/isEmailTaken/${control.value}`).pipe(
+//    map((result: boolean) => result ? { 'emailIsForbidden': true } : null),
+//   catchError(() => of(null))
+// );
+// }
+
+
+
+
+
+
+
